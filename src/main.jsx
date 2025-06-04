11396efa5847844252eb96c7d23fb9b0c630227a
Melrobayo22 <melrobayo22@Gmail.com>
2025-06-04 09:14:23 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import RobayoLabs from './RobayoLabs.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RobayoLabs />
+  </React.StrictMode>
+);
