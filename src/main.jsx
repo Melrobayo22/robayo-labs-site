616cc2c4a3aabedc1a5d03ac19bf2783ff9f32de
Melrobayo22 <melrobayo22@Gmail.com>
2025-06-04 08:53:34 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RobayoLabs from './RobayoLabs.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RobayoLabs />
-  </React.StrictMode>
-);
